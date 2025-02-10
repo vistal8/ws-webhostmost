5beec227c07db3612c71ee40399fb716dc22a285
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-02-10 13:12:49 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env, ctx) {
     let url = new URL(request.url);
     if(url.pathname.startsWith('/')){
-      url.hostname="translate.google.com"; // 修改成自己的节点IP/域名
+      url.hostname="xxx.freewebhostmost.com"; // 修改成自己的节点IP/域名
       let new_request = new Request(url, request)
       return await fetch(new_request)
     }
